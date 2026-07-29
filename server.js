ecebeb068e73e98f6be28d21ee8c4354cddc5749
szipu77 <punkoadam@gmail.com>
2026-07-29 14:09:14 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,20 +3,25 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-app.use(express.static(__dirname));
-
 const server = http.createServer(app);
+const io = new Server(server);
 
-const io = new Server(server, {
-  cors: { origin: "*" }
-});
+let lampOn = false;
+
+app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  socket.on("send", (msg) => {
-    io.emit("receive", msg);
+  console.log("User connected");
+
+  socket.emit("state", lampOn);
+
+  socket.on("toggle", () => {
+    lampOn = !lampOn;
+    io.emit("state", lampOn);
   });
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log("Fut a szerver");
 });
